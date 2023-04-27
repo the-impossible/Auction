5c8282555b640c565f481964c2b9c65d0e6da38f
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-04-27 01:34:15 +0100

    authentication successful

--- Auction_auth/views.py
+++ Auction_auth/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.views import View
 
 # Create your views here.
 from Auction_auth.forms import *
@@ -16,8 +18,36 @@ class DashboardPageView(TemplateView):
     template_name = "backend/dashboard.html"
 
 
-class LoginPageView(TemplateView):
-    template_name = "backend/auth/login.html"
+class LoginPageView(View):
+    def get(self, request):
+        return render(request, 'backend/auth/login.html')
+
+    def post(self, request):
+        email = request.POST.get('email').strip()
+        password = request.POST.get('password').strip()
+
+        if email and password:
+            user = authenticate(request, email=email, password=password)
+
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, f"You are now signed in {user}")
+
+                    nxt = request.GET.get('next', None)
+                    if nxt is None:
+                        return redirect('auth:dashboard')
+                    return redirect(self.request.GET.get('next', None))
+
+                else:
+                    messages.warning(
+                        request, 'Account not active contact the administrator')
+            else:
+                messages.error(request, 'Invalid login credentials')
+        else:
+            messages.error(request, 'All fields are required!!')
+
+        return redirect('auth:login')
 
 
 class RegisterPageView(SuccessMessageMixin, CreateView):
